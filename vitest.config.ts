30a16ed7de2b736ee36515b874112581637bb252
happy wang <shuizhongyueming@gmail.com>
2025-12-30 10:51:46 +0800

    更新依赖，使用 pnpm 来作为包管理器

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [vue()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
-})
+});
